9893c7ea1b744a68671903b531dfad0bd9a41aed
binbinmods <binbinmods@gmail.com>
2025-10-14 18:01:28 -0400

    Added gitignore

--- obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CharacterToReplace
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "com.binbin.CharacterToReplace";
-        public const string PLUGIN_NAME = "CharacterToReplace";
-        public const string PLUGIN_VERSION = "0.1.0";
-    }
-}

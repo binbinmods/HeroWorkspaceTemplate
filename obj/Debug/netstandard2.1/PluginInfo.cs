a2378db430f1c5c15f246c0f5355a056c5196114
binbinmods <binbinmods@gmail.com>
2025-10-14 17:59:58 -0400

    Update for AtO v1.6.22

---
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace CharacterToReplace
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "com.binbin.CharacterToReplace";
+        public const string PLUGIN_NAME = "CharacterToReplace";
+        public const string PLUGIN_VERSION = "0.1.0";
+    }
+}
